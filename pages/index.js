65514b8f265a90f53f3f1a1cb0e06b421904686c
Brayan Medina <brayanmedina@MacBook-Pro-de-Brayan.local>
2021-06-07 11:29:53 -0600

    test 2

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,17 @@ export default function Home() {
               `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+            h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+            (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+            })(window,document.documentElement,'async-hide','dataLayer',4000,
+            {'OPT-TQ7N4PM':true});
+              `,
+          }}
+        />
       </Head>
 
       <main className={styles.main}>
